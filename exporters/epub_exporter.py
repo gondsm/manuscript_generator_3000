b5301aa854cee1d30485ed1f968aac6b33998843
Gonçalo S. Martins <gondsm@gmail.com>
2026-01-25 09:53:02 +0000

    [WIP] Make epub able to export with parts properly

--- exporters/epub_exporter.py
+++ exporters/epub_exporter.py
@@ -1,11 +1,33 @@
 from pathlib import Path
 import subprocess
 import logging
+from textwrap import dedent
+import os
 
 from ..manuscript import Manuscript
 from . import markdown_exporter_innards
 
 logger = logging.getLogger(__name__)
+
+PANDOC_CSS = dedent("""
+    /* Page break before each top-level section */
+    section.level1 {
+        page-break-before: always;
+        break-before: page;
+    }
+
+    /* Page break after the section title */
+    section.level1 > h1 {
+        page-break-after: always;
+        break-after: page;
+        text-align: center;
+        margin-top: 40vh;
+        margin-bottom: 0;
+    }
+
+""").strip()
+
+TEMP_CSS_FILE = "./temp.css"
 
 
 def export(manuscript: Manuscript,
@@ -20,21 +42,29 @@ def export(manuscript: Manuscript,
     * out_file will be used as the file to output the epub into.
     starts.
     """
-
-    # TODO: for the moment, we don't support parts when exporting to epub, we we tell the markdown side of the exporter
-    # to straight-up ignore them.
-    markdown_content = markdown_exporter_innards.convert_content_to_lines(manuscript, ignore_parts=True)
+    markdown_content = markdown_exporter_innards.convert_content_to_lines(manuscript)
     pandoc_input = markdown_exporter_innards.concatenate_content_lines_into_string(markdown_content).encode('utf-8')
+
+    # So here's the deal: I couldn't find a way to have parts with a clear page that didn't involve CSS. Now I bucket
+    # CSS into the same realm as all those new-fangled things that kids these days are doing with the interwebs, and
+    # thus have a bit of a visceral desire never to touch it. With pandoc forcing my hand, however, I tried to keep
+    # it circumscribed, lest my mind be taken over by the idea of centring divs or something. SO! CSS is written out
+    # to a temporary file, which is nuked right after pandoc is called. This also gives me a nice, neat way of
+    # adding more CSS later if I need to. Oh how the mighty have fallen.
+    with open(TEMP_CSS_FILE, "w", encoding="utf-8") as f:
+        f.write(PANDOC_CSS)
 
     # This whole thing revolves around pandoc
     pandoc_cmd = ["pandoc",
                   "-f", "markdown",
                   "-t", "epub",
                   "--number-sections",
+                  "--top-level-division=chapter",
                   f"--metadata=title:{manuscript.config.title}",
                   f"--metadata=author:{manuscript.config.author}",
                   f"--metadata=date:{manuscript.config.time}",
-                  "-o", str(out_file)]
+                  "-o", str(out_file),
+                  f"--css", TEMP_CSS_FILE]
 
     # Add a cover if it exists
     if manuscript.config.cover:
@@ -53,3 +83,6 @@ def export(manuscript: Manuscript,
         print("Command failed with return code", e.returncode)
         print("stdout:", e.stdout.decode('utf-8') if e.stdout else "")
         print("stderr:", e.stderr.decode('utf-8') if e.stderr else "")
+
+    if os.path.exists(TEMP_CSS_FILE):
+            os.remove(TEMP_CSS_FILE)
